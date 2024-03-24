3fc329cd7cfb8b522c6edc1e8978b390d761bba7
rjavier97 <rjavier.suarez@hotmail.com>
2024-03-23 23:23:45 -0300

    taller2 debugging terminado

--- semana1/taller2/taller2/src/main/java/aed/Debugging.java
+++ semana1/taller2/taller2/src/main/java/aed/Debugging.java
@@ -6,20 +6,21 @@ class Debugging {
     }
 
     boolean iguales(int[] xs, int[] ys) {
-        boolean res = true;
-
-        for (int i = 0; i < xs.length; i++) {
-            if (xs[i] != ys[i]) {
-                res = false;
+        boolean res = false;
+        if (xs.length == ys.length) {
+            res = true;
+            for (int i = 0; i < xs.length; i++) {
+                if (xs[i] != ys[i]) {
+                    res = false;
+                }
             }
-        }
+        }    
         return res;
     }
 
-
     boolean ordenado(int[] xs) {
         boolean res = true;
-        for (int i = 0; i < xs.length; i++) {
+        for (int i = 0; i < xs.length-1; i++) {
             if (xs[i] > xs [i+1]) {
                 res = false;
             }
@@ -28,19 +29,17 @@ class Debugging {
     }
 
     int maximo(int[] xs) {
-        int res = 0;
-        for (int i = 0; i <= xs.length; i++) {
-            if (xs[i] > res) res = i;
+        int res = xs[0];
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] >= res) res = xs[i];
         }
         return res;
     }
 
     boolean todosPositivos(int[] xs) {
-        boolean res = false;
+        boolean res = true;
         for (int x : xs) {
-            if (x > 0) {
-                res = true;
-            } else {
+            if (x <= 0) {
                 res = false;
             }
         }
